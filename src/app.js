ec0ec2016d0590169666260df1dadb98f22e2e82
Miadil <kerkeb@gmail.com>
2023-03-25 13:37:29 +0100

    variable d'environement setings

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const router = require("./router");
+const { required } = require("joi");
+const corsOptions = {
+  origin: ["*", "*"],
+  methods: ["GET", "POST"],
+};
 
 const app = express();
+app.use(cors(corsOptions));
 // 2
+app.use(express.json());
+//3
+
+app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.status(200).send("on et la !");
+});
 app.use("/api", router);
 // 1
 app.get("*", (req, res) => {
