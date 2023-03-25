2591e86260d7c0ba085d6644ae17eb4195498c7f
Miadil <kerkeb@gmail.com>
2023-03-25 15:53:07 +0100

    on fix main

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,9 @@ const authRoutes = require("./authRoutes");
 const authorization = require("../middleware/auth");
 
 const router = express.Router();
-
+app.get("/", (req, res) => {
+  res.status(200).send("on et la ! /api");
+});
 router.use("/movie", authorization, movieRoutes);
 router.use("/user", userRoutes);
 router.use("/auth", authRoutes);
